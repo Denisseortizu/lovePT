d28dee536c337cdec5c4f68c52f9ecc2196e7914
Denisse Ortiz <denisseortizu@gmail.com>
2024-02-27 13:04:37 -0600

    Initial Creation of files

---
+++ lovePT.py
@@ -0,0 +1,13 @@
+"""
+Author: Denisse Ortiz
+Date of creation: 27/02/2024
+Last modification: 27/02/2024
+Description: Program for writting love letters out of given txt
+
+Other Information:
+- Version: 1.0
+- Contact: hello.denisseortiz@example.com
+"""
+
+print ("Hello World")
+
